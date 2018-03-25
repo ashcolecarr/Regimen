3fdaedff0b1f46278f8c9181492bc4ab91ddbece
ashcolecarr <ashcolecarr@gmail.com>
2018-03-25 18:28:35 -0500

    Added lists

--- Regimen/Regimen/ViewModel/MainViewModel.cs
+++ Regimen/Regimen/ViewModel/MainViewModel.cs
@@ -2,33 +2,43 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
+using System.Reflection;
 using System.Text;
+using System.Xml.Serialization;
+using Xamarin.Forms;
 
 namespace Regimen.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        public string TitleWithCurrentDate => $"Exercise Regimen for {DateTime.Now.ToString("dddd, MMMM d")}";
+        public string CurrentDate => DateTime.Now.ToString("dddd, MMMM d");
 
-        public List<Exercise> Exercises
+        private List<Exercise> _exercises;
+
+        public List<Exercise> Exercises => GetExercises();
+
+        /// <summary>
+        /// Get the list of exercises for the current day.
+        /// </summary>
+        /// <returns>The current day's exercises</returns>
+        private List<Exercise> GetExercises()
         {
-            get
+            if(_exercises == null)
             {
-                List<Exercise> exercises = new List<Exercise>();
-                exercises.Add(new Exercise
+                Assembly assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainViewModel)).Assembly;
+
+                string currentDay = DateTime.Now.DayOfWeek.ToString();
+                Stream stream = assembly.GetManifestResourceStream($"Regimen.Exercises.{currentDay}.xml");
+
+                using (StreamReader reader = new StreamReader(stream))
                 {
-                    Name = "Running",
-                    Description = "Move rapidly.",
-                    Routine = "A lot"
-                });
-                exercises.Add(new Exercise
-                {
-                    Name = "Jumping",
-                    Description = "Leap high.",
-                    Routine = "A few"
-                });
-                return exercises;
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Exercise>));
+                    _exercises = (List<Exercise>)serializer.Deserialize(reader);
+                }
             }
+
+            return _exercises;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
